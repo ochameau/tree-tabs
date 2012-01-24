0e8dc0ea8636a0ea0810ff1fdba70bc6490a3002
Alexandre Poirot <poirot.alex@gmail.com>
2012-01-24 22:57:22 +0100

    Code cleanup, follow jetpack code guideline.

--- tests/test-chrome-mod.js
+++ tests/test-chrome-mod.js
@@ -1,36 +1,39 @@
 "use strict";
 
-var { Ci, Cc } = require("chrome");
-var { ChromeMod } = require("chrome-mod");
-var { windowIterator } = require("window-utils");
+const { Ci, Cc } = require("chrome");
+const { ChromeMod } = require("chrome-mod");
+const { windowIterator } = require("api-utils/window-utils");
 
 /* Tests for the ChromeMod APIs */
 
 exports.testChromeMod = function(test) {
   test.waitUntilDone();
-  
+
   let chromeMod = new ChromeMod({
-      type: "navigator:browser",
-      
-      contentScript: 'new ' + function WorkerScope() {
-        document.documentElement.setAttribute("chrome-mod-ok", "true");
-        self.on("message", function (data) {
-          if (data=="hi") self.postMessage("bye");
-        });
-      },
-      
-      onAttach: function(worker) {
-        worker.on("message", function (data) {
-          test.assertEqual(data, "bye", "get message from content script");
-          // Search for this modified window
-          for(let win in windowIterator()) {
-            if (win.document.documentElement.getAttribute("chrome-mod-ok") == "true")
-              return test.done();
+    type: "navigator:browser",
+
+    contentScript: 'new ' + function WorkerScope() {
+      document.documentElement.setAttribute("chrome-mod-ok", "true");
+      self.on("message", function (data) {
+        if (data=="hi")
+          self.postMessage("bye");
+      });
+    },
+
+    onAttach: function(worker) {
+      worker.on("message", function (data) {
+        test.assertEqual(data, "bye", "get message from content script");
+        // Search for this modified window
+        for(let win in windowIterator()) {
+          if (win.document.documentElement.getAttribute("chrome-mod-ok") == "true") {
+            test.done();
+            return;
           }
-          test.fail("Unable to found the modified window, with 'chrome-mod-ok' attribute");
-        });
-        worker.postMessage("hi");
-      }
-    });
-  
+        }
+        test.fail("Unable to found the modified window, with 'chrome-mod-ok' attribute");
+      });
+      worker.postMessage("hi");
+    }
+  });
+
 };
--- tests/test-main.js
+++ tests/test-main.js
@@ -3,16 +3,3 @@ const main = require("main");
 exports.test_test_run = function(test) {
   test.pass("Unit test running!");
 };
-/*
-exports.test_open_tab = function(test) {
-  const tabs = require("tabs");
-  tabs.open({
-    url: "http://www.mozilla.org/",
-    onReady: function(tab) {
-      test.assertEqual(tab.url, "http://www.mozilla.org/");
-      test.done();
-    }
-  });
-  test.waitUntilDone(20000);
-};
-*/
